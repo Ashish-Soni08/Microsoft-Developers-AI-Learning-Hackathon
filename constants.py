d8dfbedee70bdca9b0108179f720c7ba4bf227f9
Ashish Soni <ashish.soni2091@gmail.com>
2024-06-22 19:44:25 +0000

    add models

--- constants.py
+++ constants.py
@@ -2,5 +2,12 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-ENDPOINT = config["AZURE_AI_ENDPOINT"]
 API_KEY = config["OPENAI_API_KEY"]
+
+API_VERSION = "2024-02-01"
+
+ENDPOINT = config["AZURE_AI_ENDPOINT"]
+
+EMBEDDING_MODEL = "text-embedding-ada-002"
+
+LLM = "gpt-4-32k"
