027abab69bb4ab2272a815d7dc5a8b4c2c95f6ce
Ashish Soni <ashish.soni2091@gmail.com>
2024-06-23 22:34:07 +0000

    update

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import sys
 
 from langchain_community.vectorstores import AzureCosmosDBVectorSearch
-from langchain_openai import (AzureChatOpenAI, AzureOpenAIEmbeddings)
+from langchain_openai import AzureChatOpenAI
+from langchain_core.messages import HumanMessage
+
+from openai import AzureOpenAI
+
+import pymongo
 
 from constants import (AZURE_OPENAI_ENDPOINT,
                        AZURE_OPENAI_API_KEY,
@@ -12,41 +17,28 @@ from constants import (AZURE_OPENAI_ENDPOINT,
                        EMBEDDING_MODEL,
                        LLM)
 
-llm_client = AzureOpenAI(
-    azure_endpoint=ENDPOINT,
-    api_key=API_KEY,
-    api_version=API_VERSION,
+
+# LLM
+llm = AzureChatOpenAI(            
+        temperature = 0.2,
+        openai_api_version = AZURE_OPENAI_API_VERSION,
+        azure_endpoint = AZURE_OPENAI_ENDPOINT,
+        openai_api_key = AZURE_OPENAI_API_KEY,         
+        model_name = LLM
+        )
+
+# Embeddings
+client = AzureOpenAI(
+    azure_endpoint=AZURE_OPENAI_ENDPOINT,
+    api_key=AZURE_OPENAI_API_KEY,
+    api_version=AZURE_OPENAI_API_VERSION,
 )
 
-MESSAGES = [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {
-        "role": "assistant",
-        "content": "The Los Angeles Dodgers won the World Series in 2020.",
-    },
-    {"role": "user", "content": "Write a Poem"},
-]
-
-
-try:
-    llm_response = llm_client.chat.completions.create(
-        model=LLM,
-        messages=MESSAGES,
-        stream=True
-    )
-
-    print(llm_response.model_dump_json(indent=2))
-
-except Exception as error:
-    print(error.response.text)
-    # sys.exit()
-
-embeddings = llm_client.embeddings.create(
+embeddings = client.embeddings.create(
     input = "Hey I am Ashish",
     model= EMBEDDING_MODEL
 )
 
 
-
-    
+# Connect to the database
+client = pymongo.MongoClient(DB_CONNECTION)
