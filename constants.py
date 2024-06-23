e9822e42fe4aeddb6cd18f305b1db9024997ad98
Ashish Soni <ashish.soni2091@gmail.com>
2024-06-23 22:29:44 +0000

    update

--- constants.py
+++ constants.py
@@ -2,11 +2,16 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-API_KEY = config["OPENAI_API_KEY"]
+AZURE_OPENAI_ENDPOINT = config["AZURE_AI_ENDPOINT"]
+AZURE_OPENAI_API_KEY = config["OPENAI_API_KEY"]
 
-API_VERSION = "2024-02-01"
 
-ENDPOINT = config["AZURE_AI_ENDPOINT"]
+AZURE_OPENAI_API_VERSION = "2024-02-01"
+
+AZURE_OPENAI_CHAT_DEPLOYMENT_NAME = "chat"
+AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME = "embeddings"
+
+DB_CONNECTION = config["DB_CONNECTION_STRING"]
 
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
