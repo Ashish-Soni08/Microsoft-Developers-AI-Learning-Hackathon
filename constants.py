a25fc8b8456119d9e0bc02e4851163b4b3939426
Ashish Soni <ashish.soni2091@gmail.com>
2024-06-25 23:27:20 +0000

    update

--- constants.py
+++ constants.py
@@ -2,17 +2,22 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
+# Azure OpenAI Endpoint, API Key, and API Version
 AZURE_OPENAI_ENDPOINT = config["AZURE_AI_ENDPOINT"]
 AZURE_OPENAI_API_KEY = config["OPENAI_API_KEY"]
-
-
 AZURE_OPENAI_API_VERSION = "2024-02-01"
 
-AZURE_OPENAI_CHAT_DEPLOYMENT_NAME = "chat"
-AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME = "embeddings"
-
+# Azure Cosmos DB for MongoDB (vCore) Connection String
 DB_CONNECTION = config["DB_CONNECTION_STRING"]
 
+# MODEL NAMES
 EMBEDDING_MODEL = "text-embedding-ada-002"
-
 LLM = "gpt-4-32k"
+
+# Llamaparse for Parsing PDFs 
+LLAMAPARSE_API_KEY = config["LLAMAPARSE_API"]
+
+# LangSmith for Tracing and Monitoring
+LANGCHAIN_URL = config["LANGCHAIN_ENDPOINT"]
+LANGCHAIN_API_KEY = config["LANGCHAIN_API"]
+LANGCHAIN_PROJECT = config["LANGCHAIN_PROJECT"]
